3efb012c7f96b2d5e47270632cdf4b9e4b79b1b8
Anton Voronov <anton.voronov@intel.com>
2023-07-14 20:25:33 +0000

    [FORK][FEATURE] jit_uni_reorder: relaxed isa condition to enable FP16 precision support on AVX2 platforms
    The feature is required to enable fast FP16->BF16/FP32 precision conversion. Most commonly used for fast weights decompression which improves model compilation time.

--- src/cpu/x64/jit_uni_reorder.cpp
+++ src/cpu/x64/jit_uni_reorder.cpp
@@ -179,7 +179,7 @@ struct jit_uni_reorder_kernel_f32_t : public kernel_t, public jit_generator {
                 && IMPLICATION(utils::one_of(bf16, p.itype, p.otype),
                         mayiuse(avx512_core) || mayiuse(avx2_vnni_2))
                 && IMPLICATION(utils::one_of(f16, p.itype, p.otype),
-                        mayiuse(avx512_core_fp16) || mayiuse(avx2_vnni_2))
+                        mayiuse(avx512_core_fp16) || mayiuse(avx2))
                 && prb_has_small_strides(p);
 
         return ok;
@@ -275,7 +275,7 @@ struct jit_uni_reorder_kernel_f32_t : public kernel_t, public jit_generator {
                                       vcvtph2psx(dst, src);
                                   else
                                       vcvtph2psx(dst, Xmm(src.getIdx()));
-                              } else if (is_superset(isa_, avx2_vnni_2)) {
+                              } else if (is_superset(isa_, avx2)) {
                                   if (src.isMEM())
                                       vcvtph2ps(dst, src);
                                   else
